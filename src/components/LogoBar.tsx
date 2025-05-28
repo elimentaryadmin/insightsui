594c6e27d13b01b20db3a2f2e7a8902486f5bbfa
MannatSaini <mannat.saini@gmail.com>
2025-05-28 21:26:33 +0530

    changed the log file

--- src/components/LogoBar.tsx
+++ src/components/LogoBar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function LogoBar() {
   return (
     <Image
-      src="/elimentary_logo.png"
+      src="/elimentary_logo.svg"
       alt="Elimentary"
       width={125}
       height={30}
